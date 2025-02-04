f882c074ac11aeeadf2d1e64253cbf24adfc450e
DeadLaurin <73538816+DeadLaurin@users.noreply.github.com>
2025-02-04 01:54:39 +0100

    Update compress_video.sh

--- compress_video.sh
+++ compress_video.sh
@@ -18,7 +18,6 @@
 #
 #=============================================================================
 
-# show the usage pattern of this script
 function usage()
 {
     echo "Usage: $(basename $0) [-e EXTENSION] [-s SOURCE] [-d DESTINATION] [-b BITRATE]" 2>&1
@@ -29,18 +28,15 @@ function usage()
     exit 1
 }
 
-# unset variables
 unset -v extension
 unset -v source
 unset -v destination
 unset -v bitrate
 
-# if no input argument found, exit the script with usage
 if [[ ${#} -eq 0 ]]; then
     usage
 fi
 
-# draw a dashed line the width of the console
 function draw_line()
 {
     local width=$(tput cols)
@@ -48,12 +44,10 @@ function draw_line()
     echo -e "\e[0m"
 }
 
-# list of arguments expected in the input
 optstring=":e:s:d:b:"
 
 bitrate=2000
 
-# assign arguments to variables
 while getopts ${optstring} arg; do
   case "${arg}" in
     e)
@@ -79,7 +73,6 @@ while getopts ${optstring} arg; do
   esac
 done
 
-# check that all arguments was supplied
 if [ -z "$extension" ]; then
     echo "Extension argument is required!"
     usage
@@ -93,29 +86,23 @@ if [ -z "$destination" ]; then
     usage
 fi
 
-# Enable globstar for recursive matching
 shopt -s globstar
 
-# Loop though all sub-folders and files from the source folder.
 for i in "$source"/**/*."$extension"; do
     relative_path="${i#$source}"
 
-    # Check if the destination file already exists.
     if [[ -f "$destination$relative_path" ]]; then
         echo -e "\e[1;33mDestination file exists: $destination$relative_path"
         continue
     fi
 
-    # Extract the codex used in the source file
     codex=$(ffprobe -hide_banner -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$i")
 
-    # Check if the source file is already compressed with h265. (No need to re-compress)
     if [ "${codex//$'\r'/}" = "hevc" ]; then
         echo -e "\e[1;33mSource file is already HEVC encoded : $i"
         continue
     fi
 
-    # Extract the video dimensions from the source file
     width=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=width -of default=nw=1:nk=1 "$i")
     height=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=height -of default=nw=1:nk=1 "$i")
 
@@ -124,12 +111,9 @@ for i in "$source"/**/*."$extension"; do
     echo -e "Compressing \e[1;32m""$i""\e[0m with size \e[1;31m" $width "x" $height "\e[0m and with \e[1;31m" $frames "\e[0m frames to file \e[1;34m""$destination""""$relative_path""\e[0m"
     draw_line
 
-    # Create the output folder if it doesn't exist
     mkdir -p "$destination""$(dirname "$relative_path")"
 
-    # Set ffmpeg options based on the bitrate
     ffmpeg_opts="-c:v libx265 -vtag hvc1 -b:v ${bitrate}k"
 
-    # Run ffmpeg with nice to not hog all CPU for itself
     nice ffmpeg -stats -hide_banner -loglevel error -i "$i" ${ffmpeg_opts} -map 0 -c:a copy -c:s copy "$destination$relative_path"
 done
