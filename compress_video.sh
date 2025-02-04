1cf2e8d16fcf30855b793ef2baf7d065f3a6d570
DeadLaurin <73538816+DeadLaurin@users.noreply.github.com>
2025-02-04 02:16:04 +0100

    Update compress_video.sh
    Added conversion for audio to aac format

--- compress_video.sh
+++ compress_video.sh
@@ -5,13 +5,15 @@
 #    FILE: compress_video.sh
 #
 #    USAGE:
-#       compress_video.sh [-e EXTENSION] [-s SOURCE] [-d DESTINATION] [-b BITRATE]
+#       compress_video.sh [-e EXTENSION] [-s SOURCE] [-d DESTINATION] [-b BITRATE] [-a AUDIO_OPTIONS]
 #                          -e EXTENSION      Specify the file extension to filter on source. Eg: -e mkv'
 #                          -s SOURCE         Specify the source folder to compress from. Eg: /mnt/myvideos'
 #                          -d DESTINATION    Specify the destination folder to compress to. Note that files will not be overwritten. Eg: /mnt/converted'
-#                          -b BITRATE        Specify the bitrate quality in kbps. Eg: -b 2000'
+#                          -b BITRATE        Specify the video bitrate quality in kbps. Eg: -b 2000'
+#                          -a AUDIO_OPTIONS  Specify audio re-encoding options. Eg: -a "192 6ch" for AAC 5.1 at 192 kbps'
 #
 #    DESCRIPTION: This script compresses videos to x265 (HEVC) from one location to another recursively without overwriting the destination.
+#                 It also allows re-encoding all audio streams to a specified format (e.g., AAC 5.1 at 192 kbps).
 #
 #          BUGS: Report bugs to Dead Laurin via Github:
 #                https://github.com/DeadLaurin/CompressVideo/issues
@@ -20,11 +22,12 @@
 
 function usage()
 {
-    echo "Usage: $(basename $0) [-e EXTENSION] [-s SOURCE] [-d DESTINATION] [-b BITRATE]" 2>&1
+    echo "Usage: $(basename $0) [-e EXTENSION] [-s SOURCE] [-d DESTINATION] [-b BITRATE] [-a AUDIO_OPTIONS]" 2>&1
     echo '    -e EXTENSION      Specify the file extension to filter on source. Eg: -e mkv'
     echo '    -s SOURCE         Specify the source folder to compress from. Eg: /mnt/myvideos'
     echo '    -d DESTINATION    Specify the destination folder to compress to. Note that files will not be overwritten. Eg: /mnt/converted'
-    echo '    -b BITRATE        Specify the bitrate quality in kbps. Eg: -b 2000'
+    echo '    -b BITRATE        Specify the video bitrate quality in kbps. Eg: -b 2000'
+    echo '    -a AUDIO_OPTIONS  Specify audio re-encoding options. Eg: -a "192 6ch" for AAC 5.1 at 192 kbps'
     exit 1
 }
 
@@ -32,6 +35,7 @@ unset -v extension
 unset -v source
 unset -v destination
 unset -v bitrate
+unset -v audio_options
 
 if [[ ${#} -eq 0 ]]; then
     usage
@@ -44,7 +48,7 @@ function draw_line()
     echo -e "\e[0m"
 }
 
-optstring=":e:s:d:b:"
+optstring=":e:s:d:b:a:"
 
 bitrate=2000
 
@@ -61,6 +65,9 @@ while getopts ${optstring} arg; do
         ;;
     b)
         bitrate=$OPTARG
+        ;;
+    a)
+        audio_options=$OPTARG
         ;;
     :)
         echo "$0: Must supply an argument to -$OPTARG." >&2
@@ -115,5 +122,24 @@ for i in "$source"/**/*."$extension"; do
 
     ffmpeg_opts="-c:v libx265 -vtag hvc1 -b:v ${bitrate}k"
 
-    nice ffmpeg -stats -hide_banner -loglevel error -i "$i" ${ffmpeg_opts} -map 0 -c:a copy -c:s copy "$destination$relative_path"
+    if [ -n "$audio_options" ]; then
+        audio_bitrate=$(echo "$audio_options" | awk '{print $1}')
+        audio_channels=$(echo "$audio_options" | awk '{print $2}' | sed 's/ch//')
+
+        if ! [[ "$audio_channels" =~ ^[0-9]+$ ]]; then
+            echo "Invalid audio channels: $audio_channels"
+            exit 1
+        fi
+
+        ffmpeg_audio_opts="-c:a aac -b:a ${audio_bitrate}k -ac ${audio_channels}"
+    else
+        ffmpeg_audio_opts="-c:a copy"
+    fi
+
+    nice ffmpeg -stats -hide_banner -loglevel error -i "$i" \
+        ${ffmpeg_opts} \
+        -map 0:v -map 0:a -map 0:s? \
+        ${ffmpeg_audio_opts} \
+        -c:s copy \
+        "$destination$relative_path"
 done
